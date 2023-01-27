f8358b192dd461ad30a5e81f12a9a624d570003e
Olexandr Marchuk <seachsic@gmail.com>
2023-01-27 22:17:41 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import axios from 'axios';
 import { fetchImages } from './js/fetchImages';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
